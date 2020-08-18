dd585a244e48a1db7e31c198fd7bfae56f8d3907
Michael <maestromike5@gmail.com>
2020-08-17 20:41:35 -0400

    added a line, ignore this

--- web_scraper.py
+++ web_scraper.py
@@ -7,3 +7,4 @@ headers = {
 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
 }
 req = requests.get(url, headers)
+soup = bs.get(req.content)
