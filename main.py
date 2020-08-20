81566c9c687dacbc5814d406a5936b90e87a0a1c
VoxelVortex <maestromike5@gmail.com>
2020-08-19 23:01:03 -0400

    Added broad exception catches to keep the program from stopping if it loses its internet connection or something else happens

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import time
 
 srl = setup_serial()
 while 1:
-    state = parse_state(get_state())
-    commit(state, srl)
-    time.sleep(3600)
+    try:
+        state = parse_state(get_state())
+        commit(state, srl)
+        time.sleep(3600)
+    except Exception as e:
+        print("An error occurred, waiting a few seconds and trying again")
+        time.sleep(30)
