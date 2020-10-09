547efed2451425899f37d914459b514539242d4e
Michael <maestromike5@gmail.com>
2020-10-09 17:27:44 -0400

    moved main program logic into a main function;added file and function comments to explain what everything does more clearly

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+"""
+Author: Michael Scalzetti (github.com/voxelvortex)
+main.py
+
+This file serves as the main logic that runs everything.
+"""
 from web_scraper import *
 from serial_comms import *
 import time
 
 
-srl = setup_serial()
-while 1:
-    try:
-        state = parse_state(get_state_bsre())
-        commit(state, srl)
-        time.sleep(3600)
-    except Exception as e:
-        print("An error occurred, waiting a few seconds and trying again")
-        time.sleep(30)
+def main():
+    srl = setup_serial()
+    while 1:
+        try:
+            state = parse_state(get_state_bsre())
+            commit(state, srl)
+            time.sleep(3600)
+        except Exception as e:
+            print("An error occurred, waiting a few seconds and trying again")
+            time.sleep(30)
+
+
+if __name__ == "__main__":
+    main()
