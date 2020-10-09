fe6d5ccfc4a10db50e6e11d0b9b13f026e6fca6b
Michael <maestromike5@gmail.com>
2020-10-09 14:32:36 -0400

    changed get_state to be get_state_xml; added new function called get_state_bsre which uses beautifulsoup and re to display what the current alert level is at RIT. This method actually works properly, which is why I changed it; get_state_xml is deprecated, but I'm leaving it temporarily in case everything breaks and I need to test my code

--- web_scraper.py
+++ web_scraper.py
@@ -1,8 +1,11 @@
 import requests
 from lxml import html
+import re
+from bs4 import BeautifulSoup as bs
 
 
-def get_state():
+def get_state_xml():
+    #depricated get_state that uses xml paths
     url = "https://www.rit.edu/ready/rit-covid-19-alert-levels"
     headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
@@ -13,6 +16,31 @@ def get_state():
     tree = html.fromstring(req.content)
     span = tree.xpath(span_xpath)
     return str(span[0].text_content())
+
+
+def get_state_bsre():
+    # new get_state function that uses beautifulsoup and regex
+    codes = {
+      "11754": "Green (Low Risk with Vigilance)",
+      "11773": "Yellow (Low to Moderate Risk)",
+      "11776": "Orange (Moderate Risk)",
+      "11779": "Red (High Risk)"
+    }
+    url = "https://www.rit.edu/ready/rit-covid-19-alert-levels"
+    headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+    }
+    req = requests.get(url, headers)
+    soup = bs(req.text, 'html.parser')
+    divs = soup.find_all('div', {"class": "single-column-container"})
+
+    for div in divs:
+        reg = re.search("<div class=\"single-column-container single-column-container-[0-9]{5} page-row position-relative border-bottom-0 black-jumbo (?!d-none)", str(div))
+        if not reg:
+            continue
+
+        code = re.search("[0-9]{5}", reg[0])[0]
+        return codes[code]
 
 
 def parse_state(state):
